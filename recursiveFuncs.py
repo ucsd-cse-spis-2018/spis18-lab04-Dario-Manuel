3623e0b30eb85e8f5f6d1e3235cd6b125f40fdb8
thatbob9001 <spis18bs@its-cseb240-19.ucsd.edu>
2018-08-15 14:42:06 -0700

    MU/DA Multiplication with recursion

--- recursiveFuncs.py
+++ recursiveFuncs.py
@@ -1,8 +1,9 @@
 # Implement a function that multiply integers without using the '*' operator
-
 def recProduct(a, b):
-   """ Returns the product of two integers a and b without using the * operator
-       This function should use recursion"""
-   
-   return 'stub'
-
+    ''' Returns the product of two integers a and b without using the * operator
+    This function should use recursion '''
+    if b == 0:
+        return 0
+    elif b > 0:
+        return recProduct(a, b-1) + a
+    return 'stub'
--- test_recursiveFuncs.py
+++ test_recursiveFuncs.py
@@ -1,17 +1,23 @@
 # test_recursiveFuncs.py
 
 import unittest
-from recursiveFuncs import recProduct 
+from recursiveFuncs import recProduct
 
 
 class Test_recProduct(unittest.TestCase):
-    
+
     def test_recProduct_1(self):
         self.assertAlmostEqual(recProduct(0,5),0)
+    def test_recProduct_2(self):
+        self.assertAlmostEqual(recProduct(-1,5),-5)
+    def test_recProduct_3(self):
+        self.assertAlmostEqual(recProduct(100,6),600)
+    def test_recProduct_4(self):
+        self.assertAlmostEqual(recProduct(3,9),27)
 
     # Note to students: add more of these test cases
- 
-        
-        
+
+
+
 if __name__ == '__main__':
     unittest.main()
