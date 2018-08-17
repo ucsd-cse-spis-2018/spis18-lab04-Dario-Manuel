7692c156253a91728b4a778dcd92f2537e2ec05c
thatbob9001 <spis18bs@its-cseb230-19.ucsd.edu>
2018-08-17 13:30:19 -0700

    Worked out the kinks

--- snowflake.py
+++ snowflake.py
@@ -1,44 +1,32 @@
 import turtle
 t = turtle.Turtle()
-def snowflake( length, level, t ):
+t.speed(0)
+def snowflake(length, level, t):
     angle = 60
     segment = length/3
-    if level == 0:
+    if level <= 0:
         t.fd(segment)
         return
     else:
+        snowflake(segment, level - 1, t)
         t.rt(60)
-        t.fd(segment)
-        snowflake( segment, level - 1, t )
-        t.fd(segment)
-        #t.lt(120)
-        
+        snowflake(segment, level - 1, t)
         t.lt(120)
-        t.fd(segment)
-        snowflake( segment, level - 1, t )
+        snowflake(segment, level - 1, t)
         t.rt(60)
-        
+        snowflake(segment, level - 1, t)
 
-def snowflakeLauncher( length, level, t ):
-    snowflake( length, level, t )
+def snowflakeLauncher( length, level, t):
+    snowflake(length, level, t)
     t.lt(120)
-    snowflake( length, level, t )
+    snowflake(length, level, t)
     t.lt(120)
-    snowflake( length, level, t )
-    t.lt(120) 
-        
-##
-##        t.rt(60)
-##        t.fd(segment)
-##        snowflake( segment, level - 1, t )
-##        t.rt(120)
-##        t.fd(segment)
-##        t.lt(120)
+    snowflake(length, level, t)
+    t.lt(120)
 
-        #t.fd(segment)
-        #snowflake( segment, level - 1, t )
-        #t.fd(segment)
-        #t.lt(120)
-
-
-snowflakeLauncher( 100, 4, t ) 
+t.back(300)
+snowflakeLauncher(1500, 0, t)
+snowflakeLauncher(1500, 1, t)
+snowflakeLauncher(1500, 2, t)
+snowflakeLauncher(1500, 3, t)
+snowflakeLauncher(1500, 4, t)
